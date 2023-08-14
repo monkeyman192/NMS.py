994b8528d8d65ecae9c056ff03d13000757de78a
monkeyman192 <monkey_man_192@yahoo.com.au>
2023-08-14 23:08:05 +1000

    Better fix for console window

--- nmspy/logging.py
+++ nmspy/logging.py
@@ -17,8 +17,8 @@ class stdoutSocket():
 
 def open_log_console(log_script: str) -> int:
     """ Open the logging console and return the pid of it."""
-    cmd = 'cmd.exe /c start "NMS.py" python ' + log_script
-    with subprocess.Popen(cmd, shell=True) as proc:
+    cmd = ["cmd.exe", "/c", "start", "NMS.py console", "python", log_script]
+    with subprocess.Popen(cmd) as proc:
         log_ppid = proc.pid
     for proc in psutil.process_iter(["pid", "name", "ppid"]):
         if proc.info["ppid"] == log_ppid:
